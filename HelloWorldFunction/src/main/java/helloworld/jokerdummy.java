4c5ecc91effc60dca99f32ad6fd43dbd07ba941a
liamhyde02 <73969644+liamhyde02@users.noreply.github.com>
2024-05-26 23:51:51 -0700

    i am going to become the joker

---
+++ HelloWorldFunction/src/main/java/helloworld/jokerdummy.java
@@ -0,0 +1,8 @@
+package helloworld;
+
+public class jokerdummy {
+    /*This is a dummy class to test my workflow */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
