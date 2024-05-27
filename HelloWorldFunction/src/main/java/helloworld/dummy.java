f5f51761c854363f83304393fe93e5751c8b8647
liamhyde02 <73969644+liamhyde02@users.noreply.github.com>
2024-05-27 01:28:30 -0700

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Create dummy.java""""""""""""""

---
+++ HelloWorldFunction/src/main/java/helloworld/dummy.java
@@ -0,0 +1,6 @@
+public class dummy {
+    /*This is a dummy class to test whether the workflow works on push! */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
